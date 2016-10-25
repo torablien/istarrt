a1660acc8b15279c8678308f8a45c73fd5e435e7
Neil <neilbarot@gmail.com>
2016-10-25 06:02:53 +0000

    built layout for project pages

--- home/static/airspace/js/google-map-init.js
+++ home/static/airspace/js/google-map-init.js
@@ -1,24 +1,25 @@
 jQuery(document).ready(function() {
     "use strict";
 
-
     function b() {
         var a = {
                 zoom: 16,
                 scrollwheel: false,
-                center: new google.maps.LatLng(29.9399014,-90.1218437),
-                styles: [{
-                    "featureType": "landscape",
-                }]
+                center: new google.maps.LatLng(29.9399014, -90.1218437),
+                styles: [{"featureType": "landscape",}]
             },
+            
             b = document.getElementById("map"),
+            
             c = new google.maps.Map(b, a);
+            
         new google.maps.Marker({
-            position: new google.maps.LatLng(29.9399014,-90.1218437),
+            position: new google.maps.LatLng(29.9399014, -90.1218437),
             map: c,
             title: "Snazzy!"
         })
+        
     }
+    
     google.maps.event.addDomListener(window, "load", b);
-
 });
