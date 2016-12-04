865268e6a7324712ab9b0204fa642055fa0c6dac
peterhriser <peterhriser@gmail.com>
2016-12-04 23:02:05 +0000

    Linekd DB to Projects model, created loop to pull from db in index

--- home/models.py
+++ home/models.py
@@ -3,3 +3,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class projects(models.Model):
+    title = models.CharField(max_length = 200)
+    subtitle = models.TextField(max_length = 200)
+    images = models.TextField(max_length=500)
+    video = models.TextField(max_length=200)
+    words = models.TextField(max_length = 1000)
+    filepath = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.title
+    
+
